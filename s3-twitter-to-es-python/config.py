89d7d681275c18d42eab032c227e55aac3bcade3
dhall <dhall006@odu.edu>
2019-08-23 13:50:50 -0400

    All files for Stream API approach

---
+++ s3-twitter-to-es-python/config.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 12, 2015
+
+@author: mentzera
+
+'''
+
+es_host = 'search-fedception-es-ynqxrgpew2wwkvou7pfrdkieki.us-east-1.es.amazonaws.com' #without the https - for example: search-es-twitter-demo-xxxxxxxxxxxxxxxxxxx.us-east-1-.es.amazonaws.com
+es_port = 80
+es_bulk_chunk_size = 1000  #number of documents to index in a single bulk operation
+
