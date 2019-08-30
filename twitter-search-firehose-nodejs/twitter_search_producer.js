c3b35a0bae7f38da6cb03af534b9ead6f611378e
Separ <E1MXS04@rb.win.frb.org>
2019-08-30 09:59:45 -0400

    adding package twitter-search-firehose-nodejs

---
+++ twitter-search-firehose-nodejs/twitter_search_producer.js
@@ -0,0 +1,148 @@
+/***
+Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Amazon Software License (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+
+http://aws.amazon.com/asl/
+
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+***/
+
+//'use strict';
+
+var config = require('C:/temp/config');
+var Twitter = require('twitter');
+var util = require('util');
+var logger = require('./util/logger');
+
+function twitterStreamProducer(firehose) {
+  var log = logger().getLogger('producer');
+  var waitBetweenPutRecordsCallsInMilliseconds = config.waitBetweenPutRecordsCallsInMilliseconds;
+  var T = new Twitter(config.twitter)
+
+  // Creates a new kinesis stream if one doesn't exist.
+  function _createStreamIfNotCreated(callback) {
+    firehose.describeDeliveryStream({DeliveryStreamName: config.firehose.DeliveryStreamName}, function(err, data) {
+      if (err) {
+        firehose.createDeliveryStream(config.firehose, function(err, data) {
+          if (err) {
+            // ResourceInUseException is returned when the stream is already created.
+            if (err.code !== 'ResourceInUseException') {
+              console.log(err);
+              callback(err);
+              return;
+            }
+            else {
+              var msg = util.format('%s stream is already created! Re-using it.', config.firehose.DeliveryStreamName);
+              console.log(msg);
+              log.info(msg);
+            }
+          }
+          else {
+            var msg = util.format('%s stream does not exist. Created a new stream with that name.', config.firehose.DeliveryStreamName);
+            console.log(msg);
+            log.info(msg);
+          }
+          // Poll to make sure stream is in ACTIVE state before start pushing data.
+          _waitForStreamToBecomeActive(callback);
+        });
+      }
+      else {
+        var msg = util.format('%s stream is already created! Re-using it.', config.firehose.DeliveryStreamName);
+        log.info(msg);
+      }
+
+      // Poll to make sure stream is in ACTIVE state before start pushing data.
+      _waitForStreamToBecomeActive(callback);
+    });
+
+    
+  }
+
+  // Checks current status of the stream.
+  function _waitForStreamToBecomeActive(callback) {
+    firehose.describeDeliveryStream({DeliveryStreamName: config.firehose.DeliveryStreamName}, function(err, data) {
+      if (!err) {
+        if (data.DeliveryStreamDescription.DeliveryStreamStatus === 'ACTIVE') {
+          log.info('Current status of the stream is ACTIVE.');
+          callback(null);
+        }
+        else {
+          var msg = util.format('Current status of the stream is %s.', data.DeliveryStreamDescription.DeliveryStreamStatus);
+          console.log(msg);
+          log.info(msg);
+          setTimeout(function() {
+            _waitForStreamToBecomeActive(callback);
+          }, 1000 * config.waitBetweenDescribeCallsInSeconds);
+        }
+      }
+    });
+  }
+
+
+  function _sendToFirehose() {
+
+
+    var params = {
+      q: 'Federal Reserve',
+      count: 100
+    }
+
+    var records = [];
+    var record = {};
+    var recordParams = {};
+
+    T.get('search/tweets', params, function (err, data) {
+      if (err) {
+        console.log("Error = " + JSON.stringify(err));
+      }
+     
+      var tweets = data.statuses;
+
+      for (const tweet of tweets) {
+        recordParams = {
+          DeliveryStreamName: config.firehose.DeliveryStreamName,
+          Record: {
+            Data: JSON.stringify(tweet)+',\n'
+          }
+        };
+
+        log.info("Tweet = " + JSON.stringify(recordParams));
+
+
+        firehose.putRecord(recordParams, function(err, data) {
+
+          log.info("Firehose put response = " + JSON.stringify(data));
+          if (err) {
+            log.error(err);
+          }
+        });
+      } 
+
+     
+    });
+  }
+
+
+  return {
+    run: function() {
+      log.info(util.format('Configured wait between consecutive PutRecords call in milliseconds: %d',
+          waitBetweenPutRecordsCallsInMilliseconds));
+      
+      _createStreamIfNotCreated(function(err) {
+        if (err) {
+          log.error(util.format('Error creating stream: %s', err));
+          return;
+        }
+        _sendToFirehose();
+      });
+    }
+  };
+}
+
+module.exports = twitterStreamProducer;
